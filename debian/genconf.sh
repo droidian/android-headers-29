5129b708034a9c2fce345902499a42c902b46bc6
Bhushan Shah <bhush94@gmail.com>
2017-09-07 10:51:51 +0530

    Fix the configuration step

--- debian/genconf.sh
+++ debian/genconf.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 sed '/\/\* CONFIG GOES HERE/,$d' android-config.h > android-config.h.new
-cat <<EOF >> android-config-caf.h
+cat <<EOF >> android-config.h.new
 #define QCOM_BSP 1
 #define QTI_BSP 1
 EOF
